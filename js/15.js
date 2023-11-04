de39b15b544a9576dc4890a9b3834fa9b59f4efa
QuemoZem <fran.tucmascotas@gmail.com>
2023-11-04 14:36:06 -0300

    termine ejercicio 15

---
+++ js/15.js
@@ -0,0 +1,13 @@
+/** Realiza un script que cuente el número de vocales que tiene un texto */
+
+let contador = 0;
+let vocales = ["a", "e", "i", "o", "u"];
+let ingresoTexto = prompt("Ingrese un texto");
+let textoEnMinusculas = ingresoTexto.toLocaleLowerCase();
+
+for (let i = 0; i <= textoEnMinusculas.length; i++) {
+  if (vocales.includes(textoEnMinusculas[i])) {
+    contador++;
+  }
+}
+document.write(`Cantidad de vocales en la frase : ${contador}`);
