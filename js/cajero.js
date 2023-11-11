966c3f324bafe060d88f6e6de812b4413ee4cd6b
QuemoZem <fran.tucmascotas@gmail.com>
2023-11-11 01:15:09 -0300

    Agregando validaciones y mejoras al ejercicio

--- js/cajero.js
+++ js/cajero.js
@@ -1,27 +1,57 @@
 /** Ejercicio extra */
 
-let opcion = prompt(
-  `Ingrese una opcion : \n 1)- Consultar Saldo \n 2)- Ingresar Dinero \n 3)- Extraer Dinero`
-);
 let saldo = 0;
 let extraccion = 0;
 
+let opcion = prompt(
+  `Ingrese una opcion:\n1) Consultar Saldo\n2) Ingresar Dinero\n3) Extraer Dinero`
+);
+
 switch (opcion) {
   case "1":
-    document.write(` Su saldo es de ${saldo}`);
+    consultarSaldo();
     break;
   case "2":
-    saldo = parseInt(prompt("cuanto dinero desea ingresar?"));
-    document.write(`Su saldo actual es : ${saldo}`);
+    ingresarDinero();
     break;
   case "3":
-    extraccion = parseInt(prompt("Cuanto dinero desea Extraer?"));
-    if (saldo > 0 && saldo > extraccion) {
-      saldo = saldo - extraccion;
-    } else {
-      alert("No hay plata amigo");
-    }
+    extraerDinero();
     break;
   default:
     alert("La opcion ingresada es incorrecta");
+}
+
+function consultarSaldo() {
+  document.write(`Su saldo es de ${saldo}`);
+}
+
+function ingresarDinero() {
+  let montoIngreso = parseInt(
+    prompt("Ingrese la cantidad de dinero a ingresar:")
+  );
+
+  if (!isNaN(montoIngreso) && montoIngreso > 0) {
+    saldo += montoIngreso;
+    document.write(`Su saldo actual es: ${saldo}`);
+  } else {
+    alert("Ingrese un monto válido.");
+  }
+}
+
+function extraerDinero() {
+  let montoExtraccion = parseInt(
+    prompt("Ingrese la cantidad de dinero a extraer:")
+  );
+
+  if (
+    !isNaN(montoExtraccion) &&
+    montoExtraccion > 0 &&
+    saldo >= montoExtraccion
+  ) {
+    saldo -= montoExtraccion;
+  } else if (saldo < montoExtraccion) {
+    alert("No hay suficiente saldo para realizar la extracción.");
+  } else {
+    alert("Ingrese un monto válido.");
+  }
 }
